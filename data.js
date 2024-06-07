e2143883844a4d1b35bc79b242bc164e5e9486bd
Aastha010503 <aasthasingh75518@gmail.com>
2024-06-07 21:29:00 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,34 @@
+let songs = [
+    {
+        name: "Apna Bana Le",
+        artist: "Arijit Singh",
+        path: "songs/Arijit Singh- Apna Bana Le.mp3",
+        cover: "images/Apna Bana Le.jpg"
+    },
+    {
+        name: "You Belong With Me",
+        artist: "Taylor Swift",
+        path: "songs/Taylor Swift - You Belong With Me.mp3",
+        cover: "images/you belong with me.jpg"
+    },
+    {
+        name: "Best Song Ever",
+        artist: "One Direction",
+        path: "songs/One Direction - Best Song Ever (Audio).mp3",
+        cover: "images/best song ever.jpg"
+    },
+    {
+        name: "Yellow Lights",
+        artist: "Harry Hudson",
+        path: "songs/Harry Hudson - Yellow Lights (Lyrics).mp3",
+        cover: "images/yellow lights.jpg"
+    },
+   
+    {
+        name:"Counting Stars",
+        artist:"One Republic",
+        path:"songs/OneRepublic - Counting Stars (Lyrics).mp3",
+        cover:"images/Counting Stars.jpeg"
+    }
+
+]
